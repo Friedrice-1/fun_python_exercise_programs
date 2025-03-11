b0bfef5f5b784ada6859160a0e9a29b7f33ce091
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 03:40:04 +0800

    Print output

--- which_number_is_bigger.py
+++ which_number_is_bigger.py
@@ -5,3 +5,10 @@ num_2 = int(input("Please enter the second  number: "))
 
 # check which number is bigger
 # print the bigger number
+
+if num_1 > num_2:
+    print(f'{num_1} is greater than {num_2}')
+elif num_2 > num_1:
+    print(f'{num_2} is greater than {num_1}')
+else:
+    print('Numbers are equal.')
