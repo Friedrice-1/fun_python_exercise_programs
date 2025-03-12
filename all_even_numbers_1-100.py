b9e0a8cf3b9a4d9d78c4da8839090bbfc22e6408
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:49:41 +0800

    Print results

--- all_even_numbers_1-100.py
+++ all_even_numbers_1-100.py
@@ -1,5 +1,7 @@
 # loop print 100 times
+
 for numbers in range(101):
-    print(numbers)
 # check if number is even or odd
+    if numbers % 2 == 0 or numbers == 0:
+        print(numbers)
 # print result
