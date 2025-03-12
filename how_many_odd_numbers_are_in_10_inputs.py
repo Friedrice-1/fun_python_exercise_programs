eaaf3e65d1028fdbe23a52054662945329519bd2
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:42:36 +0800

    10 number inputs

---
+++ how_many_odd_numbers_are_in_10_inputs.py
@@ -0,0 +1,10 @@
+# initialize odd count variable
+
+odd_count = 0
+
+# loop inputs 10 times
+
+for inputs in range(10):
+    numbers = int(input(f"Please enter number {inputs+1}: "))
+# count how many are odd
+# print result
