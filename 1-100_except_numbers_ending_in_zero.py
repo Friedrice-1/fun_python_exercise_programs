0761be209bb431d404d6423babe3ea848c1adb26
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:55:35 +0800

    Print all numbers

--- 1-100_except_numbers_ending_in_zero.py
+++ 1-100_except_numbers_ending_in_zero.py
@@ -1,6 +1,7 @@
 # loop print 100 times
 
 for numbers in range(101):
-    print(numbers)
 # check if number is ending in zero or zero
+    if numbers % 10 != 0:
 # print result
+        print(numbers)
