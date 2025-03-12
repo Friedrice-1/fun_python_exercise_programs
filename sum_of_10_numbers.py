89482c634542a1bd1a1db494c29eb36fb9357957
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:37:05 +0800

    10 inputs and total sum variable

--- sum_of_10_numbers.py
+++ sum_of_10_numbers.py
@@ -1,4 +1,11 @@
 # initialize total sum variable
+
+total_sum = 0
+
 # loop inputs 10 times
+
+for inputs in range(10):
+    numbers = int(input(f"Please enter number {inputs+1}: "))
+
 # increment the inputs to the total sum variable
 # print result
