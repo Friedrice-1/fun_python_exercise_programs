4743457fe1d8faeaf479108a843d5d9eb5ab467b
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:30:01 +0800

    2 number inputs and instructions

--- a_number_is_raised_to_another_number.py
+++ a_number_is_raised_to_another_number.py
@@ -1,3 +1,9 @@
 # input 2 numbers
+
+print("1st number is raised to the 2nd number")
+
+num_1 = int(input("Please enter the first number: "))
+num_2 = int(input("Please enter the exponent: "))
+
 # raise the first number by the second number
 # print result
