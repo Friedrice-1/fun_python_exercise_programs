1e320e1003886a96ab6ac11c138aa95f2502fc95
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:39:03 +0800

    Prints result of adding 10 numbers

--- sum_of_10_numbers.py
+++ sum_of_10_numbers.py
@@ -6,6 +6,8 @@ total_sum = 0
 
 for inputs in range(10):
     numbers = int(input(f"Please enter number {inputs+1}: "))
-
 # increment the inputs to the total sum variable
+    total_sum += numbers
+
 # print result
+print(f"The sum of 10 numbers are {total_sum}")
