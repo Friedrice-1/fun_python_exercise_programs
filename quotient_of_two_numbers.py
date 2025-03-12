f4af50b002e9bab1856bc610a278b6c998a0fa7c
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-12 14:25:27 +0800

    2 number inputs dividend and divisor

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -1,3 +1,8 @@
 # input 2 numbers
+
+print("dividend/divisor")
+dividend = float(input("Please enter the dividend: "))
+divisor = float(input("Please enter the divisor: "))
+
 # divides the 2 numbers
 # prints the result
