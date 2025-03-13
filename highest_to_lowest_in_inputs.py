d6dc974306e5c3892130330d7be5a8d6ec5f1b40
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 14:22:42 +0800

    Loops inputs and appends input to the list

--- highest_to_lowest_in_inputs.py
+++ highest_to_lowest_in_inputs.py
@@ -1,5 +1,15 @@
 # initialize list
+
+numbers = []
+
 # loop inputs until invalid
+
+while True:
+    try:
+        number =  int(input("Please enter a number: "))
 # append input to the list
+        numbers.append(number)
 # check if input is invalid
+    except ValueError:
+        break
 # print list from highest to lowest
