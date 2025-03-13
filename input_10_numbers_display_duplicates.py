75e072a4faf646348837e410a15ec83186896b82
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:48:17 +0800

    Pseudocode

---
+++ input_10_numbers_display_duplicates.py
@@ -0,0 +1,5 @@
+# create a list for user input and duplicate numbers
+# loop input 10 times and list them
+# count how many numbers are in the list
+# store duplicate numbers in user input list to the duplicate number list
+# print duplicate numbers
