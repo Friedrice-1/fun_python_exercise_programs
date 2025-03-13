7a70841ba5759abb1fdc91ba34f5472764ecf2dd
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 12:52:49 +0800

    Added 10 inputs that store into a list

--- input_10_numbers_display_all_duplicates_only_display_first_occurence.py
+++ input_10_numbers_display_all_duplicates_only_display_first_occurence.py
@@ -1,5 +1,14 @@
 # create a list for numbers input
+numbers = []
 # create an empty set to store the numbers
+seen_numbers = set()
+
 # loop input 10 times
+
+for inputs in range(10):
+    number = int(input(f"Number {inputs + 1}: "))
+    numbers.append(number)
+
+print(numbers)
 # check if inputs numbers have occured
 # print output
