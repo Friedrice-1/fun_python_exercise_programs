5accf9073957d5b1d3e85aba8fdcf6d7a1e7fef2
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:49:33 +0800

    loops input 10 times and appends them to the list

--- input_10_numbers_display_duplicates.py
+++ input_10_numbers_display_duplicates.py
@@ -1,5 +1,14 @@
 # create a list for user input and duplicate numbers
+
+numbers =  []
+duplicate_numbers = []
+
 # loop input 10 times and list them
+
+for inputs in range(10):
+    number = int(input(f"Please enter number {inputs + 1}: "))
+    numbers.append(number)
+
 # count how many numbers are in the list
 # store duplicate numbers in user input list to the duplicate number list
 # print duplicate numbers
