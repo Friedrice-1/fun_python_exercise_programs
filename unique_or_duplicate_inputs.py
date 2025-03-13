a557178e441688f23d31fa5bd1752a0a1d7a742f
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:23:38 +0800

    Added append to the numbers list

--- unique_or_duplicate_inputs.py
+++ unique_or_duplicate_inputs.py
@@ -1,16 +1,18 @@
 # initialize number list
 
-number = []
+numbers = []
 
 # loop inputs until invalid
 
 while True:
     try:
-        num = int(input("Please enter a number: "))
+        number = int(input("Please enter a number: "))
+# append input to the number list
+        numbers.append(number)
     except:
         ValueError
         break
 
-# append input to the number list
+print(numbers)
 # check if input is a duplicate or unique
 # if input is invalid the loop stops
