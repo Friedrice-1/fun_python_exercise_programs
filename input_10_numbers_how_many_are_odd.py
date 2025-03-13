6431700ece8fec4757ddfdfa8a69acb9ba42e063
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:28:51 +0800

    Loop inputs

--- input_10_numbers_how_many_are_odd.py
+++ input_10_numbers_how_many_are_odd.py
@@ -1,4 +1,11 @@
 # initialize odd number count variable
+
+even_number = 0
+
 # loop inputs 10 times
+
+for i in range(10):
+    numbers = int(input(f"Please enter number {i + 1}: "))
+
 # increment the input to the odd number count variable if the input is odd
 # print result
