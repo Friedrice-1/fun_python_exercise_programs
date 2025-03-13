638b3a17f26758b565d49bf9bf421851e661d88d
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 14:13:33 +0800

    Counts the frequency of each input and prints the most duplicate

--- input_numbers_until_invalid_print_the_most_duplicates.py
+++ input_numbers_until_invalid_print_the_most_duplicates.py
@@ -13,7 +13,13 @@ while True:
     except ValueError:
         break
 
-print(numbers)
 # make dictionary for counting numbers
+count_dict = {}
+
 # find the most repeated number
+for number in numbers:
+        count_dict[number] = count_dict.get(number, 0) + 1
+most_frequent = max(count_dict, key=count_dict.get)
+
 # print result
+print(most_frequent)
