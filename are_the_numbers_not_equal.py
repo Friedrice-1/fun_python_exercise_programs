4cbeea7032d7571817d2facb9a4274a493f767a4
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 10:55:13 +0800

    Inputs 2 numbers

--- are_the_numbers_not_equal.py
+++ are_the_numbers_not_equal.py
@@ -1,3 +1,7 @@
 # input 2 numbers
+
+num_1 = int(input("Please enter the first  number: "))
+num_2 = int(input("Please enter the second  number: "))
+
 # checks if numbers are not equal
 # print result
