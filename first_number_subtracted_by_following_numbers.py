8027c8764c7ea024944a8b0d4873517b9db3b626
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 11:12:14 +0800

    Added all the inputs

--- first_number_subtracted_by_following_numbers.py
+++ first_number_subtracted_by_following_numbers.py
@@ -1,2 +1,8 @@
 # ask user to input initial number
+
+init_number = int(input("Please enter the first number: "))
+
 # loop inputs 9 times then subtract the inputs from the initial number
+
+for number in range(1,9):
+    numbers = float(input(f"Please enter number {number + 1}: "))
