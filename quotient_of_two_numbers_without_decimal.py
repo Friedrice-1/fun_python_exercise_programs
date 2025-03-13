d63849a4a81b86bfc6fd4746171685677442c8f8
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 11:04:49 +0800

    Added instructions and inputs 2 numbers

--- quotient_of_two_numbers_without_decimal.py
+++ quotient_of_two_numbers_without_decimal.py
@@ -1,3 +1,8 @@
 # input 2 numbers
+
+print("dividend/divisor")
+dividend = int(input("Please enter the dividend: "))
+divisor = int(input("Please enter the divisor: "))
+
 # divides the 2 numbers
 # prints the result
