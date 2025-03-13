73b86822bdcac5d9bc568a19ca3a8cec50594131
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 10:53:11 +0800

    Fixed Pseudocode and Added Output

--- which_number_is_smaller.py
+++ which_number_is_smaller.py
@@ -3,5 +3,12 @@
 num_1 = int(input("Please enter the first  number: "))
 num_2 = int(input("Please enter the second  number: "))
 
-# check which number is bigger
-# print the bigger number
+# check which number is smaller
+# print the smaller number
+
+if num_1 < num_2:
+    print(f"{num_1} is smaller than {num_2}")
+elif num_2 < num_1:
+    print(f"{num_2} is smaller than {num_1}")
+else:
+    print("Numbers are equal.")
