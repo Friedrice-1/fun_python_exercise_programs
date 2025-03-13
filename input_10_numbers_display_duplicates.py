2b4ecbbf6129d1d777174d745ba8b4f4fa05315a
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:53:06 +0800

    Prints duplicate numbers

--- input_10_numbers_display_duplicates.py
+++ input_10_numbers_display_duplicates.py
@@ -10,5 +10,9 @@ for inputs in range(10):
     numbers.append(number)
 
 # count how many numbers are in the list
+for number in numbers:
+    if numbers.count(number) > 1 and number not in duplicate_numbers:
 # store duplicate numbers in user input list to the duplicate number list
+        duplicate_numbers.append(number)
 # print duplicate numbers
+print(f"The numbers that are duplicate/s are: {duplicate_numbers}")
