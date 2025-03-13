71bec711c1f042006dd9e1b1e30eb5919f4ee4fc
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 11:20:57 +0800

    Prints result

--- input_10_numbers_how_many_are_even.py
+++ input_10_numbers_how_many_are_even.py
@@ -5,8 +5,10 @@ even_number = 0
 # loop inputs 10 times
 
 for i in range(10):
-    numbers = int(input(f"Please enter number {i+1}: "))
+    numbers = int(input(f"Please enter number {i + 1}: "))
 # increment the input to the even number count variable if the input is even
-    if numbers%2 == 0:
+    if numbers % 2 == 0:
         even_number += 1
+
 # print result
+print(f"There are {even_number} even numbers.")
