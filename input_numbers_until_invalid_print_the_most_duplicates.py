2bb68381539485c58908e0cebe351611dd9bfc8e
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 14:06:12 +0800

    Pseudocode

---
+++ input_numbers_until_invalid_print_the_most_duplicates.py
@@ -0,0 +1,7 @@
+# make a list for numbers input
+# loop until invalid
+# append input to number list
+# check if input is invalid
+# make dictionary for counting numbers
+# find the most repeated number
+# print result
