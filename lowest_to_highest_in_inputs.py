c7b11844d31aca8d933dfce8d9f4462c05597ad5
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:41:37 +0800

    Looped inputs until input is invalid

--- lowest_to_highest_in_inputs.py
+++ lowest_to_highest_in_inputs.py
@@ -1,5 +1,14 @@
 # initialize list
+
+numbers = []
+
 # loop inputs until invalid
+
+while True:
+    try:
+        number =  int(input("Please enter a number: "))
 # append input to the list
 # check if input is invalid
+    except ValueError:
+        break
 # print list from lowest to highest
