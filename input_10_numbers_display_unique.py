9e5e55641f861597c715d25e02ebc22e3e1280be
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 12:32:16 +0800

    Made a list for the 10 number inputs

--- input_10_numbers_display_unique.py
+++ input_10_numbers_display_unique.py
@@ -1,5 +1,16 @@
 # create a list for user input and unique numbers
+
+numbers =  []
+unique_numbers = []
+
 # loop input 10 times and list them
+
+for inputs in range(10):
+    number = int(input(f"Please enter number {inputs + 1}: "))
+    numbers.append(number)
+
+print(numbers)
+
 # count how many numbers are in the list
 # store unique numbers in user input list to the unique number list
 # print unique numbers
