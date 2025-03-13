de336f3c82ab4a612d3c1c5f858c06d1ac4f0674
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 14:29:16 +0800

    Loop inputs

--- input_until_invalid_print_average.py
+++ input_until_invalid_print_average.py
@@ -1,4 +1,14 @@
 # initialize input count
 # initialize sum variable
+
+input_count = 0
+total_sum = 0
+
 # loop inputs until invalid
+
+while True:
+    try:
+        number = int(input("Please enter a number: "))
+    except ValueError:
+        break
 # print average
