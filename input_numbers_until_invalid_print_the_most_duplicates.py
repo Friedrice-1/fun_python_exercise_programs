fc572df9bb57dbd0317cba8d8cd4c15e2b57c91c
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 14:07:54 +0800

    Looped inputs and appends them to the list

--- input_numbers_until_invalid_print_the_most_duplicates.py
+++ input_numbers_until_invalid_print_the_most_duplicates.py
@@ -1,7 +1,19 @@
 # make a list for numbers input
+
+numbers = []
+
 # loop until invalid
+
+while True:
+    try:
+        number = int(input("Please enter a number: "))
 # append input to number list
+        numbers.append(number)
 # check if input is invalid
+    except ValueError:
+        break
+
+print(numbers)
 # make dictionary for counting numbers
 # find the most repeated number
 # print result
