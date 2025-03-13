63b8ff532a3aa94e297942cd9be8f0dbe2387eb1
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 11:26:23 +0800

    Added number loop

--- while_loop_odd_numbers.py
+++ while_loop_odd_numbers.py
@@ -1,4 +1,12 @@
 # initialize number variable
+
+current_number = 0
+
 # loop 100 times
+
+while True:
+    current_number += 1
+    if current_number == 101:
+        break
 # check if the current number is odd
 # print number
