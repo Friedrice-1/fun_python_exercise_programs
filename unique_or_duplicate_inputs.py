cdb53136bdcfc235acef5b8150293db7ae58f9a4
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 13:20:33 +0800

    Looped inputs

--- unique_or_duplicate_inputs.py
+++ unique_or_duplicate_inputs.py
@@ -1,5 +1,16 @@
 # initialize number list
+
+number = []
+
 # loop inputs until invalid
+
+while True:
+    try:
+        num = int(input("Please enter a number: "))
+    except:
+        ValueError
+        break
+
 # append input to the number list
 # check if input is a duplicate or unique
 # if input is invalid the loop stops
