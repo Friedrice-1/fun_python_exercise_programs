f623ecfb94dde1dd1c9be9c007e7f3922b0063a5
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-13 12:30:03 +0800

    Pseudocode

---
+++ input_10_numbers_display_unique.py
@@ -0,0 +1,5 @@
+# create a list for user input and unique numbers
+# loop input 10 times and list them
+# count how many numbers are in the list
+# store unique numbers in user input list to the unique number list
+# print unique numbers
